063735a0b2aaf99b119bae1b549ad92451b96f53
taher <tahersumonabu@gmail.com>
2024-08-09 21:46:37 +0000

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/test', function () {    
-    dd(bcrypt('123456'));
+    dd( App\Models\User::all());
 });
